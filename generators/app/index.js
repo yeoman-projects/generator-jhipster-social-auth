f9034d8489ac272ad0f938c49e5c7a209e94e140
Seif Eddine Mouelhi <seif.mouelhi@forsyslab.com>
2017-03-12 00:14:58 +0300

    java and spring configuration added to integrate social connector to JHipster

--- generators/app/index.js
+++ generators/app/index.js
@@ -63,7 +63,33 @@ module.exports = yeoman.Base.extend({
       this.log('angularAppName=' + this.angularAppName);
       this.log('message=' + this.message);
 
-      this.template('dummy.txt', 'dummy.txt', this, {});
+      this.template('src/main/java/package/config/social/_SocialConfiguration.java', javaDir + 'config/social/SocialConfiguration.java');
+      this.template('src/main/java/package/domain/_SocialUserConnection.java', javaDir + 'domain/SocialUserConnection.java');
+      this.template('src/main/java/package/repository/_CustomSocialConnectionRepository.java', javaDir + 'repository/CustomSocialConnectionRepository.java');
+      this.template('src/main/java/package/repository/_CustomSocialUsersConnectionRepository.java', javaDir + 'repository/CustomSocialUsersConnectionRepository.java');
+      this.template('src/main/java/package/repository/_SocialUserConnectionRepository.java', javaDir + 'repository/SocialUserConnectionRepository.java');
+      this.template('src/main/java/package/security/social/_CustomSignInAdapter.java', javaDir + 'security/social/CustomSignInAdapter.java');
+      this.template('src/main/java/package/security/social/_package-info.java', javaDir + 'security/social/package-info.java');
+      this.template('src/main/java/package/service/_SocialService.java', javaDir + 'service/SocialService.java');
+      this.template('src/main/java/package/web/rest/_SocialController.java', javaDir + 'web/rest/SocialController.java');
+      this.template('src/main/resources/config/liquibase/_fortunes.csv', resourceDir + 'config/liquibase/fortunes.csv');
+
+
+      // this.template('src/main/webapp/scripts/app/fortune/_fortune.controller.js', webappDir + 'scripts/app/fortune/fortune.controller.js');
+      // jhipsterFunc.addJavaScriptToIndex('app/fortune/fortune.controller.js');
+      // this.template('src/main/webapp/scripts/app/fortune/_fortune.html', webappDir + 'scripts/app/fortune/fortune.html');
+      // this.template('src/main/webapp/scripts/app/fortune/_fortune.js', webappDir + 'scripts/app/fortune/fortune.js');
+      // jhipsterFunc.addJavaScriptToIndex('app/fortune/fortune.js');
+      // this.template('src/main/webapp/scripts/components/fortune/_fortune.service.js', webappDir + 'scripts/components/fortune/fortune.service.js');
+      // jhipsterFunc.addJavaScriptToIndex('components/fortune/fortune.service.js');
+      // jhipsterFunc.addElementToMenu('fortune', 'sunglasses', true);
+      // jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'en');
+      // jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'fr');
+      //
+      // jhipsterFunc.copyI18nFilesByName(this, webappDir, 'fortune.json', 'en');
+      // jhipsterFunc.copyI18nFilesByName(this, webappDir, 'fortune.json', 'fr');
+      done();
+
     },
 
     registering: function () {
